645b84b4c6b85e615dcff79bcf94860017e2ce59
Liliana Perez <103470899+o7lili@users.noreply.github.com>
2022-08-27 22:20:40 -0400

    comment routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,10 +2,13 @@ const router = require('express').Router();
 
 const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes.js');
+const commentRoutes = require('./comment-routes');
+
 const Post = require('../../models/Post.js');
 const User = require('../../models/User.js');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;

4be22d3cd882569bcde4f588de3122ff2b0ebe94
Liliana Perez <103470899+o7lili@users.noreply.github.com>
2022-09-03 21:57:50 -0400

    added in edit and delete posts from dashboard

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/connection');
+const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
@@ -20,10 +19,11 @@ const sess = {
   })
 };
 
-const hbs = exphbs.create({ helpers });
-
 app.use(session(sess));
 
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
